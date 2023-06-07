f0dccaebfeab6017ffa3863d6ad386b742bdeb76
Daniil <hokkeict_@mail.ru>
2023-06-07 22:27:33 +0300

    Семинар от 28.05.2023

--- 2_lessons/Seminar 28.05.2023/25_Task.py
+++ 2_lessons/Seminar 28.05.2023/25_Task.py
@@ -4,23 +4,20 @@
 # Input: a a a b c a a d c d d
 # Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 
-s = 'a a a b a c b b d d'.split()
-r = ''
-
-for i, letter in enumerate((s)):
-    if s[:i].count(letter)<1:
-        r+=letter+' '
-    else:
-        r+=f'{letter}_{s[:i].count(letter)} '
-print(r)
-
-
+# a = 'a a a b c a a d c d d'.split()
+# b = ''
+#
+# for i in range (len(a)):
+#     if a[i]
 
 s = 'a a a b a c b b d'.split()
 r = ''
 for i in range(len(s)):
+    print(i)
     if s[0:i:].count(s[i]) == 0:
+        print(f's[0:i:].count(s[i]) = {s[0:i:].count(s[i])}')
         r += s[i]
+        print(f'r = {r}')
     else:
         r += f'{s[i]}_{s[0:i].count(s[i])}'
     print(s[0:i], r)
